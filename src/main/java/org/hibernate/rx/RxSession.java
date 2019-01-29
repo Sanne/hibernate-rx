2070cdfc41142965b7052a831fd047beac4b7041
Sanne Grinovero <sanne@hibernate.org>
2019-01-29 12:27:37 +0000

    Rename load() to find() on Emmanuel's suggestion to match JPA

--- src/main/java/org/hibernate/rx/RxSession.java
+++ src/main/java/org/hibernate/rx/RxSession.java
@@ -2,13 +2,15 @@ package org.hibernate.rx;
 
 import java.util.concurrent.CompletableFuture;
 
+import io.reactivex.Maybe;
+
 /**
  * Right now we are playing around, but this is going to be the core
  * interface of the project.
  */
 public interface RxSession {
 
-	<T> CompletableFuture<T> load(Class<T> entityClass, Object id);
+	<T> Maybe<T> find(Class<T> entityClass, Object id);
 
 	CompletableFuture<Void> persist(Object entity);
 
